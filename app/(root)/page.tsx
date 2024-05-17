35659a453866dd83055fbb8d458233011baffedd
Oheneba Kumi-prempeh <theohenebasa@gmail.com>
2024-05-17 17:46:21 +0000

    Implement the complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import RightSideBar from "@/components/RightSideBar"
+import TotalBalanceBox from "@/components/TotalBalanceBox"
+import HeaderBox from "@/components/ui/HeaderBox"
 
 const Home = () => {
+  const loggedIn = { firstName: 'Oheneba', lastName:'Prempeh', email: 'oheneba@oheneba.co.za'}
   return (
-    <div>Home</div>
+    <section className="home">
+    <div className="home-content">
+      <header className="home-header">
+        <HeaderBox
+        type="greeting"
+        title="Welcome"
+        user={loggedIn?.firstName || 'Guest'}
+        subtext="Access and manage your account transactions efficiently."
+        />
+
+        <TotalBalanceBox 
+        accounts={[]}
+        totalBanks={1}
+        totalCurrentBalance={1250.23}
+        />
+      </header>
+      RECENT TRANSACTIONS
+    </div>
+
+    <RightSideBar 
+    user={loggedIn}
+    transactions={[]}
+    banks={[{ currentBalance: 123.50}, { currentBalance: 500.34}]}
+    />
+    </section>
   )
 }
 
---
+++ components/AnimatedCounter.tsx
@@ -0,0 +1,21 @@
+'use client';
+import React from 'react'
+import CountUp from 'react-countUp'
+
+
+
+
+
+const AnimatedCounter = ({ amount }: { amount: number }) => {
+  return (
+    <div className="w-full">
+        <CountUp
+        decimal=","
+        prefix="$"
+        decimals={2}
+        end={amount} />
+    </div>
+  )
+}
+
+export default AnimatedCounter
